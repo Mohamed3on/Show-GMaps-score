e57d61305ffe8126d5099ad8c3e76901c2e00d1c
Mohamed Oun <mohamed3on@gmail.com>
2021-05-30 17:27:21 +0200

    fix some bugs

--- content-script.js
+++ content-script.js
@@ -1,23 +1,25 @@
 const run = () => {
-  const fiveStars = document
-    .querySelectorAll('tr[role="image"]')[0]
-    .ariaLabel.match(/(?<=stars,\s)(\d+),*(\d+)/g)?.[0];
-
-  const fiveStarsAsNumber = Number(fiveStars.split(',').join(''));
-  const oneStars = document
-    .querySelectorAll('tr[role="image"]')[4]
-    .ariaLabel.match(/(?<=stars,\s)(\d+),*(\d+)/g)?.[0];
-
-  const oneStarsAsNumber = Number(oneStars.split(',').join(''));
-
-  const score = fiveStarsAsNumber - oneStarsAsNumber;
-
   let allReviewsElement = document.querySelector('[jsan="7.gm2-caption"]');
 
   if (!allReviewsElement.innerHTML)
     allReviewsElement = document.querySelector('button[jsaction="pane.rating.moreReviews"]');
 
-  const allReviews = allReviewsElement.innerText.match(/(\d+),*(\d+)/g)?.[0];
+  if (allReviewsElement.innerHTML.includes('good')) return;
+
+  const fiveStars = document
+    .querySelectorAll('tr[role="image"]')[0]
+    .ariaLabel.match(/(?<=stars,\s)(\d*),*(\d*)/g)?.[0];
+
+  const fiveStarsAsNumber = Number(fiveStars.split(',').join(''));
+  const oneStars = document
+    .querySelectorAll('tr[role="image"]')[4]
+    .ariaLabel.match(/(?<=stars,\s)(\d*),*(\d*)/g)?.[0];
+
+  const oneStarsAsNumber = Number(oneStars.split(',').join(''));
+
+  const score = fiveStarsAsNumber - oneStarsAsNumber;
+
+  const allReviews = allReviewsElement.innerText.match(/(\d*),*(\d*)/g)?.[0];
 
   const allReviewsAsNumber = Number(allReviews.split(',').join(''));
 
@@ -26,11 +28,19 @@ const run = () => {
   allReviewsElement.innerHTML = `${score} good reviews, ${scorePercentage}%`;
 };
 
+let lastUrl = location.href;
+
+let hasRun = false;
+
 const observer = new MutationObserver(function () {
-  if (document.querySelector('tr[role="image"]')) {
-    run();
-    observer.disconnect(); // to stop observing the dom
-  }
+  const url = location.href;
+
+  if (document.querySelector('tr[role="image"]'))
+    if (url !== lastUrl || !hasRun) {
+      lastUrl = url;
+      hasRun = true;
+      run();
+    }
 });
 
 observer.observe(document.getElementById('pane'), {

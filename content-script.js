ed6070dcc7d8e49f200230c79c6f23e63912f37f
Mohamed Oun <mohamed3on@gmail.com>
2023-03-23 13:02:54 +0100

    adapt to new UI

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
 const addCommas = (x) => {
   return x.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
-const run = (allReviewsElement) => {
+const run = (placeDetailsElement) => {
   const fiveStars = document
     .querySelectorAll('tr[role="img"]')[0]
     .ariaLabel.match(/(?<=stars,\s)(\d*),*(\d*)/g)?.[0];
@@ -15,7 +15,9 @@ const run = (allReviewsElement) => {
 
   const score = fiveStarsAsNumber - oneStarsAsNumber;
 
-  const allReviews = allReviewsElement.innerText.match(/\d*\.*,*\d*/g)?.[0];
+  const allReviews = document
+    .querySelector('button[jsaction="pane.reviewChart.moreReviews"]')
+    .innerText.match(/\d*\.*,*\d*/g)?.[0];
 
   const allReviewsAsNumber = Number(allReviews.split(/[.,]/g).join(''));
 
@@ -25,17 +27,16 @@ const run = (allReviewsElement) => {
 
   const scorePercentage = Math.round(ratio * 100);
 
-  allReviewsElement.innerHTML = `score: ${addCommas(
-    String(calculatedScore)
-  )} — ${scorePercentage}%`;
+  const newElement = document.createElement('div');
+  newElement.innerHTML = `score: ${addCommas(String(calculatedScore))} — ${scorePercentage}%`;
+  placeDetailsElement.appendChild(newElement);
 };
 
 const observer = new MutationObserver(function () {
-  const allReviewsElement = document.querySelector(
-    '[jsaction="pane.rating.moreReviews"] > span:nth-child(2) > span > span'
-  );
-  if (allReviewsElement && !allReviewsElement.innerHTML.includes('score')) {
-    run(allReviewsElement);
+  const placeDetails = document.querySelector('.dmRWX');
+
+  if (placeDetails && !placeDetails.innerHTML.includes('score')) {
+    run(placeDetails);
   }
 });
 
